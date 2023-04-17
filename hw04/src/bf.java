ad5e186d2e1d91dbc944d4b7e8fee16bdae9908c
leonardo <leo.bonadimani@finetuning.it>
2023-04-17 15:04:00 +0200

    Progress

--- hw04/src/bf.java
+++ hw04/src/bf.java
@@ -2,9 +2,11 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class bf extends bfBaseVisitor<Integer>{
     // memory list
+    private final boolean debug = false;
     private List<Integer> memory = new ArrayList<>();
     private Integer index = 0;
 
@@ -29,8 +31,11 @@ public class bf extends bfBaseVisitor<Integer>{
     public Integer visitComma(bfParser.CommaContext ctx) {
         // Try to read a character from the input stream
         try {
-            System.out.println("Input an Integer: ");
-            memory.set(index, System.in.read());
+            // Ask an integer to the user
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Input an Integer: ");
+            Integer newValue = scanner.nextInt();
+            memory.set(index, newValue);
         } catch (Exception e) {
             System.out.println("Error reading from input stream");
         }
@@ -39,7 +44,7 @@ public class bf extends bfBaseVisitor<Integer>{
 
     @Override
     public Integer visitDot(bfParser.DotContext ctx) {
-        System.out.print((char) memory.get(index).intValue());
+        System.out.println(memory.get(index));
         return visit(ctx.com());
     }
 
@@ -65,6 +70,9 @@ public class bf extends bfBaseVisitor<Integer>{
     }
 
     public Integer visitLoop(bfParser.LoopContext ctx) {
+        if (debug) {
+            System.out.println(memory);
+        }
         while (memory.get(index) != 0) {
             visit(ctx.com().get(0));
         }
